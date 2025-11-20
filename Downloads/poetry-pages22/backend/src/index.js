62db5556c383dbad4be2a273832e70677234af53
anonymousk682-maker <anonymousk682@gmail.com>
2025-11-21 00:51:19 +0530

    Update index.js

--- Downloads/poetry-pages22/backend/src/index.js
+++ Downloads/poetry-pages22/backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://katahnak-back.vercel.app/',
   credentials: true
 }));
 
