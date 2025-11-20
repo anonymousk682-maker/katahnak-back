9926c8ec9d4094f045edfda3e7b47dcdf28c75e8
kaukaran <karankaul02@gmail.com>
2025-11-21 00:04:58 +0530

    saving

---
+++ Downloads/poetry-pages22/poetry-page/frontend/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000/api';
+
+const api = axios.create({
+  baseURL: API_BASE,
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' }
+});
+
+export default api;
