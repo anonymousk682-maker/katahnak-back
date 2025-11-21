c868c71f0c297ef5527512c9c33654abcf8bec88
anonymousk682-maker <anonymousk682@gmail.com>
2025-11-21 12:33:41 +0530

    Update SiteSettings.js

--- Downloads/poetry-pages22/backend/src/models/SiteSettings.js
+++ Downloads/poetry-pages22/backend/src/models/SiteSettings.js
@@ -1,1 +1,11 @@
+import mongoose from 'mongoose';
+
+const siteSettingsSchema = new mongoose.Schema({
+  heroQuote: String,
+  featuredCardQuote: String,
+  contactEmail: String,
+  locationText: String,
+});
+
+export default mongoose.model("SiteSettings", siteSettingsSchema);
 
